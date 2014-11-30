662fd6571cbeddcc7d9a4ce43bcde020f0798747
DamirMakhmutov <damir.makhmutov@flatstack.com>
2014-11-30 03:28:17 +0300

    add post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  expose(:posts)
+  expose(:post, attributes: :post_params)
+
+  respond_to :html
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  	post.save
+
+  	respond_with post
+  end
+
+  def update
+    post.save
+
+    respond_with post
+  end
+
+  private
+
+  def post_params
+  	params.require(:post).permit(:title, :text)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'dashboard#index'
+  
+  root to: 'posts#index'
+
+  resources :posts
 end
