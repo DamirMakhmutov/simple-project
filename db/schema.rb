0f7c34103c16b90a756c52a73e342dc06a60c855
DamirMakhmutov <damir.makhmutov@flatstack.com>
2014-12-05 16:51:36 +0300

    add comment table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204130737) do
+ActiveRecord::Schema.define(version: 20141205134525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.string  "text",    default: "", null: false
+    t.integer "post_id",              null: false
+    t.integer "user_id",              null: false
+  end
 
   create_table "posts", force: true do |t|
     t.string  "title",   default: "", null: false
